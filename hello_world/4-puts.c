3d460697423369ceeb646806b9541294bfb5d244
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-04-26 11:39:39 +0000

    git add .

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 /**
  * main - Entry Point
- * 
  * Return: always 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
