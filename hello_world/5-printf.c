8af40235d7bd0f033a9bc61789eb99fb82bb6401
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-04-26 11:52:26 +0000

    Printf Function

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * mine - entry point
+ *
+ * Return: always 0
+ *
+ */
+int mine(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
