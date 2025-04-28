05be21262fc7c7d119a6e3aefe44fece076106c2
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-04-28 10:03:07 +0000

    !

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times in lower case
+ *
+ *
+ * Return:alwasy 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -17,4 +17,13 @@ int _putchar(char c);
  */
 void print_alphabet(void);
 
+/**
+ * print_alphabet_x10 - print the alphabet 10 times in lower case
+ *
+ *
+ * Return:always 0
+ *
+ */
+void print_alphabet_x10(void);
+
 #endif
