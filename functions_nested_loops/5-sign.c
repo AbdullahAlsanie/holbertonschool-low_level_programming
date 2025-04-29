23f8cf8ff586272c6f2ec1d91b890c462e363254
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-04-29 11:18:08 +0000

    !

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,24 +1,25 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * print_sign - prints the integer sign
- * @n: the integer to checke
+ * print_sign - main function
  *
- * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
+ * @n: character to compare
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
  *
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
-	putchar('-');
+	_putchar('-');
 	return (-1);
 }
