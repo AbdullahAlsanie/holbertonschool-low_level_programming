2fe7459b34b111a08bc1f2dfda5f71a2b22fcbf3
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-06 10:56:33 +0000

    fixed beety style

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0 )
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -25,7 +25,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d ",i);
+			printf("%d ", i);
 		}
 	}
 	return (0);
