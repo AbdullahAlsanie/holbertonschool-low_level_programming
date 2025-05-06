188d35956dee9318998a0239424f3b15f6f3d376
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-06 09:45:52 +0000

    fixed the logic for the functin

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j / 10 + '0');
-
-			if (j >= 10 && j <= 14)
+			if (j >= 10)
 			{
-			_putchar(j % 10 + '0');
+				_putchar('1');
 			}
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
