b3d3467b25a1b1646d27593917222f648796482d
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-08 09:51:42 +0000

    added the function reset_to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer as an input and change its value to 98
+ * @n: the input
+ *
+ * Return: always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
+ * reset_to_98 - taks a poointer as in input and change its value to 98
+ * @n: the input
+ *
+ * Return: always 0
+ */
+void reset_to_98(int *n);
+#endif
