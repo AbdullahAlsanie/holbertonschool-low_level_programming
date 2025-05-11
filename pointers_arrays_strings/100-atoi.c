f4280c460579f0b40e7d4cd3cc7bd225be8786fc
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-11 07:46:19 +0000

    fixed overflow issu and betty syle

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,24 +9,43 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int num = 0;
-    int started = 0;
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+	int started = 0;
+	int digit;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-' && !started)
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            num = num * 10 + (s[i] - '0');
-            started = 1;
-        }
-        else if (started)
-            break;
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-' && !started)
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
 
-    return sign * num;
+			if (sign == 1)
+			{
+				if (num > (2147483647 - digit) / 10)
+				{
+					return (2147483647);
+				}
+			}
+			else
+			{
+				if (num > (2147483648U - digit) / 10)
+				{
+					return (-2147483648);
+				}
+			}
+
+			num = num * 10 + digit;
+			started = 1;
+
+			started = 1;
+		}
+		else if (started)
+			break;
+		i++;
+	}
+	return (sign * num);
 }
