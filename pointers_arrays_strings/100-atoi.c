a445d8bff55faa8f8d35938137824c5a6c208924
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-11 09:30:25 +0000

    fixed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,48 +1,46 @@
 #include "main.h"
+#include "2-strlen.c"
+
 /**
- * _atoi - converts a string to an integer
- * @s: input string
+ * _atoi - converts string to integer
+ * @s: string to convert
  *
- * Return: the integer result
+ * Return: returns integer value
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
-	int digit;
-	int started = 0;
+	int i;
+	int np = 0;
+	int c;
+	int d = 1;
+	int num = 0;
 
-	while (s[i] != '\0')
+	for (i = 0; i < _strlen(s); i++)
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-
-			if (sign == 1)
-			{
-				if (result > (2147483647 - digit) / 10)
-				{
-					return (2147483647);
-				}
-			}
-			else
-			{
-				if (result > (int)((2147483648U - digit) / 10))
-					return (-2147483648);
-			}
-
-			result = result * 10 + digit;
-			started = 1;
-		}
-		else if (started)
-		{
+		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
 			break;
+		if (s[i] == '-')
+			np--;
+		if (s[i] == '+')
+			np++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			c++;
 		}
-
-		i++;
 	}
-	return (result * sign);
+	while (c > 0)
+	{
+		num += ((s[i - 1] - '0') * d);
+		i--;
+		c--;
+		d *= 10;
+	}
+	if (np >= 0)
+	{
+		num *= 1;
+	} else
+	{
+		num *= -1;
+	}
+	return (num);
 }
