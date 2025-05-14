fa641ac4370d34250e0e067460577521eeb61f03
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-14 20:08:21 +0000

    added all the functions

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -148,4 +148,61 @@ char *cap_string(char *s);
  * Return: pointer to the encoded string
  */
 char *leet(char *s);
+/**
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value of s
+ */
+char *_memset(char *s, char b, unsigned int n);
+/**
+ * _memcpy - copies from source to dest values in array
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: n amount of times
+ * Return: return the string value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+/**
+ * _strchr - locates character in string
+ * @s: string to locate char
+ * @c: character to find
+ * Return: character value
+ */
+char *_strchr(char *s, char c);
+/**
+ * _strspn - length of prefix substring
+ * @s: string to go through
+ * @accept: accepted bytes
+ * Return: returns usigned values
+ */
+unsigned int _strspn(char *s, char *accept);
+/**
+ * _strpbrk - finds string
+ * @s: string to find stuff
+ * @accept: accepting arrays
+ *
+ * Return: returns new value
+ */
+char *_strpbrk(char *s, char *accept);
+/**
+ * _strstr - locates substring
+ * @haystack: string to locte
+ * @needle: substring to locate
+ * Return: returns string at starting sub or null
+ */
+char *_strstr(char *haystack, char *needle);
+/**
+ * print_chessboard - board to print
+ * @a: prints things
+ * Return: returns crap
+ */
+void print_chessboard(char (*a)[8]);
+/**
+ * print_diagsums - prints diagonal summaries
+ * @a: array to use
+ * @size: size of diagonal
+ */
+void print_diagsums(int *a, int size);
 #endif
