c0cd990025b1490692fd9a2b0a203facc6a05376
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-21 07:56:10 +0000

    added the function malloc_checked

---
+++ more_malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
+ * malloc_checked - Allocates memory
+ * @b: Integer to allocate
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b);
+#endif
