22e5a921e4095f15281f6d318535bf47e686c11a
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-05-27 09:41:07 +0000

    fixed logic

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size != 0 && cmp != NULL)
+	if (array != NULL && size != 0 && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
