4e30fee6d9fb8260f6479639699f914813e312ae
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-06-13 04:48:39 +0000

    fixed

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -3,6 +3,9 @@
 /**
  * set_bit - sets a bit
  * @n: integer to grab
+ * @index: is the index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
  *
  */
 int set_bit(unsigned long int *n, unsigned int index)
