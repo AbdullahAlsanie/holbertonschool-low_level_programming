517844b57e2f8f20df3960af9f73dd6d66bf616e
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-06-15 10:19:05 +0000

    fixed

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,27 +8,41 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, printed, wrote;
-	char *buff;
+	int fd;
+	ssize_t r, w;
+	char *buffer;
 
-	buff = malloc(letters);
-	if (buff == NULL)
-		return (0);
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buffer);
 		return (0);
-	printed = read(fd, buff, letters);
-	if (printed == -1)
+	}
+
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
-	wrote = write(STDOUT_FILENO, buff, printed);
-	if (wrote == -1)
+	}
+
+	w = write(STDOUT_FILENO, buffer, r);
+	if (w == -1 || w != r)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
-	if (printed != wrote)
-                return (0);
-	if (close(fd) == -1)
-		return (0);
-	free(buff);
-	return (printed);
+	}
+
+	free(buffer);
+	close(fd);
+	return (w);
 }
